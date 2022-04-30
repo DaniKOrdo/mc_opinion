355a6093f13a7803ab1c07f9dcae311397f2e16e
DaniK <danikordonezgarcia@gmail.com>
2022-04-30 19:10:04 +0200

    add question and recaptcha

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,11 @@
 from ast import arguments
+from operator import truediv
 import time
 import logging
 import telegram
 import dk_token as dk
+import selenium
+from selenium.webdriver.common.action_chains import ActionChains
 from datetime import date
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,6 +13,9 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from telegram import Bot, Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 TELEGRAM_BOT_TOKEN = dk.TOKEN
 TELEGRAM_CHAT_ID = dk.CHAT_ID
@@ -22,9 +28,11 @@ actual_date = today.strftime("%d-%m-%Y")
 
 # Selenium config
 driver_options = Options()
-driver_options.add_argument('--headless')
+#driver_options.add_argument('--headless')
+# 375 667
 driver_options.add_argument('--no-sandbox')
 driver_options.add_argument("--incognito")
+driver_options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
 #driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=driver_options)
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=driver_options)
 driver.get('https://mcdonalds.fast-insight.com/voc/es/es')
@@ -74,6 +82,71 @@ def ticket(update: Update, context: CallbackContext) -> None:
 
     submit = driver.find_element('xpath', '//*[@id="welcomeMessage"]/div[4]/button')
     submit.click()
+
+    time.sleep(2)
+
+    driver.get(driver.current_url)
+
+
+    # Questions
+    question_1 = driver.find_element('xpath', '//*[@id="0"]/div[5]/div[1]')
+    question_1.click()
+
+    question_2 = driver.find_element('xpath', '//*[@id="8"]/div[5]/div[1]')
+    question_2.click()
+    
+    question_3 = driver.find_element('xpath', '//*[@id="10"]/div[5]/div[1]')
+    question_3.click()
+
+    # Scroll 1
+
+    question_4 = driver.find_element('xpath', '//*[@id="12"]/div[5]/div[1]')
+    question_4.click()
+
+    actions = ActionChains(driver)
+    actions.move_to_element(question_4).perform()
+    
+    question_5 = driver.find_element('xpath', '//*[@id="47"]/div[5]/div[1]')
+    question_5.click()
+
+    actions = ActionChains(driver)
+    actions.move_to_element(question_5).perform()
+    
+    question_6 = driver.find_element('xpath', '//*[@id="55"]/div[5]/div[1]')
+    question_6.click()
+    
+    question_7 = driver.find_element('xpath', '//*[@id="14"]/div[5]/div[1]')
+    question_7.click()
+    
+    question_8 = driver.find_element('xpath', '//*[@id="6"]/div[5]/div[1]')
+    question_8.click()
+    
+    question_9 = driver.find_element('xpath', '//*[@id="94"]/div[5]/div[1]')
+    question_9.click()
+    
+    question_10 = driver.find_element('xpath', '//*[@id="85"]/div[5]/div[2]')
+    question_10.click()
+    
+    question_11 = driver.find_element('xpath', '//*[@id="35"]/div[5]/div[1]')
+    question_11.click()
+    
+    question_12 = driver.find_element('xpath', '//*[@id="22"]/div[5]/div[2]')
+    question_12.click()
+    
+    question_13 = driver.find_element('xpath', '//*[@id="23"]/div[5]/div[2]')
+    question_13.click()
+    
+    question_14 = driver.find_element('xpath', '//*[@id="24"]/div[5]/div[4]')
+    question_14.click()
+    
+    question_15 = driver.find_element('xpath', '//*[@id="48"]/div[5]/div[2]')
+    question_15.click()
+    
+    question_16 = driver.find_element('xpath', '//*[@id="50"]/div[5]/div[2]')
+    question_16.click()
+
+    time.sleep(2)
+    
     
 
 # Escritura del bot mas lenta
